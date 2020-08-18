f32d28cd583b1d8c97cf34a3158191913b6e0917
Rahul Kabra <rahulkabra029@gmail.com>
2020-08-18 18:40:32 +0530

    added game words, style changes in footer link

---
+++ src/Constants.js
@@ -0,0 +1,439 @@
+export const AlphabetsList = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+export const CategoryList = [
+  "Colors",
+  "Shapes",
+  "Movies",
+  "Superheroes",
+  "Countries",
+  "Naruto",
+  "Flowers",
+  "Disney",
+  "HarryPotter",
+  "Music",
+  "Fantasy",
+];
+
+export const Colors = [
+  "fuchsia",
+  "salmon",
+  "firebrick",
+  "coral",
+  "moccasin",
+  "lavender",
+  "olive",
+  "teal",
+  "peachpuff",
+  "cornsilk",
+  "turquoise",
+  "sienna",
+  "cadetblue",
+  "chocolate",
+  "seashell",
+  "honeydew",
+  "ivory",
+  "papayawhip",
+  "thistle",
+  "amber",
+  "rosybrown",
+  "beige",
+  "burlywood",
+  "gold",
+  "cyan",
+  "plum",
+  "snow",
+  "wheat",
+  "tomato"
+];
+
+export const Shapes = [
+  "rhombus",
+  "cube",
+  "pentagon",
+  "cone",
+  "trapezoid",
+  "oval",
+  "ellipse",
+  "square",
+  "triangle",
+  "parallelogram",
+  "cylinder",
+  "hexagon",
+  "rectangle",
+  "circle",
+  "triangular prism",
+  "octahedron",
+  "heptagon",
+  "octagon",
+  "quadrilateral",
+  "trapezium",
+  "polygon",
+  "sphere",
+  "cuboid",
+  "pyramid",
+  "ellipsoid",
+  "nonagon",
+  "tetrahedron",
+  "kite",
+  "decagon"
+];
+
+export const Movies = [
+  "the vow",
+  "princess diaries",
+  "jumanji",
+  "the notebook",
+  "freaky friday",
+  "frozen",
+  "brave",
+  "tangled",
+  "notting hill",
+  "love actually",
+  "amelie",
+  "pretty woman",
+  "titanic",
+  "the proposal",
+  "the terminal",
+  "sully",
+  "home alone",
+  "blended",
+  "despicable me",
+  "inception",
+  "ratatouille",
+  "ghostbusters",
+  "up",
+  "finding neverland",
+  "fantastic four",
+  "coco",
+  "onward",
+  "interstellar",
+  "gravity"
+];
+
+export const Superheroes = [
+  "thor",
+  "wonder woman",
+  "superman",
+  "rogue",
+  "black panther",
+  "storm",
+  "hawkeye",
+  "captain america",
+  "ironman",
+  "black widow",
+  "starlord",
+  "scarlet witch",
+  "green lantern",
+  "batman",
+  "spiderman",
+  "antman",
+  "cyclops",
+  "flash",
+  "quicksilver",
+  "captain marvel",
+  "wolverine",
+  "aquaman",
+  "human torch",
+  "invisible woman",
+  "psylocke",
+  "falcon",
+  "the thing",
+  "hawkgirl",
+  "raven"
+];
+
+export const Countries = [
+  "spain",
+  "india",
+  "morocco",
+  "france",
+  "netherlands",
+  "russia",
+  "ireland",
+  "germany",
+  "italy",
+  "austria",
+  "czech republic",
+  "ghana",
+  "maldives",
+  "croatia",
+  "malaysia",
+  "fiji",
+  "denmark",
+  "jamaica",
+  "norway",
+  "canada",
+  "sweden",
+  "mexico",
+  "chile",
+  "brazil",
+  "iceland",
+  "new zealand",
+  "argentina",
+  "japan",
+  "portugal"
+];
+
+export const Naruto = [
+  "naruto uzumaki",
+  "rasenshuriken",
+  "susanoo",
+  "kotoamatsukami",
+  "sasuke uchiha",
+  "mangekyou sharingan",
+  "madara uchiha",
+  "chidori",
+  "konohagakure",
+  "kakashi hatake",
+  "genjutsu",
+  "jiraiya",
+  "itachi uchiha",
+  "orochimaru",
+  "hashirama senju",
+  "hokage",
+  "shikamaru nara",
+  "jinchuriki",
+  "zabuza momochi",
+  "yellow flash",
+  "sakura haruno",
+  "hinata hyuga",
+  "deidara",
+  "akatsuki",
+  "asakujaku",
+  "kazekage",
+  "gamabunta",
+  "flying raijin",
+  "kekkei genkai"
+];
+
+export const Flowers = [
+  "orchid",
+  "lily",
+  "rose",
+  "lotus",
+  "jasmine",
+  "daffodil",
+  "daisy",
+  "hibiscus",
+  "gerbera",
+  "sunflower",
+  "tulip",
+  "peony",
+  "dahlia",
+  "marigold",
+  "petunia",
+  "lavender",
+  "periwinkle",
+  "cherry blossom",
+  "bluebell",
+  "dandelion",
+  "chrysanthemum",
+  "hyacinth",
+  "snowdrop",
+  "iris",
+  "geranium",
+  "honeysuckle",
+  "hazel",
+  "mistletoe",
+  "nightshade"
+];
+
+export const Disney = [
+  "eugene",
+  "rapunzel",
+  "nemo",
+  "aladdin",
+  "jasmine",
+  "ariel",
+  "elsa",
+  "dory",
+  "simba",
+  "aurora",
+  "olaf",
+  "anna",
+  "belle",
+  "astrid",
+  "hiccup",
+  "merida",
+  "dumbo",
+  "linguini",
+  "maui",
+  "toothless",
+  "eva",
+  "mowgli",
+  "woody",
+  "alice",
+  "moana",
+  "kristoff",
+  "mater",
+  "buzz lightyear",
+  "miguel"
+];
+
+export const HarryPotter = [
+  "leaky cauldron",
+  "quidditch",
+  "marauders map",
+  "harry potter",
+  "transfiguration",
+  "boggart",
+  "remembrall",
+  "hermione granger",
+  "herbology",
+  "muggle",
+  "galleon",
+  "alohomora",
+  "animagus",
+  "ron weasley",
+  "riddikulus",
+  "yule ball",
+  "apparate",
+  "albus dumbledore",
+  "the burrow",
+  "diagon alley",
+  "severus snape",
+  "divination",
+  "expecto patronum",
+  "floo network",
+  "gillyweed",
+  "gryffindor",
+  "ravenclaw",
+  "incendio",
+  "pensieve"
+];
+
+export const Music = [
+  "acoustic",
+  "ballad",
+  "chord",
+  "chorus",
+  "edm",
+  "grunge",
+  "heavy metal",
+  "jazz",
+  "mixtape",
+  "punk rock",
+  "reggae",
+  "remix",
+  "rhythm",
+  "tambourine",
+  "turntable",
+  "verse",
+  "yodel",
+  "saxophone",
+  "cello",
+  "synthesizer",
+  "composition",
+  "pitch",
+  "opera",
+  "concert",
+  "symphony",
+  "harmonica",
+  "trumpet",
+  "violin",
+  "tempo"
+];
+
+export const Fantasy = [
+  "unicorn",
+  "lochness",
+  "apprentice",
+  "alchemy",
+  "chalice",
+  "conjure",
+  "gnome",
+  "pixie",
+  "soothsayer",
+  "dungeon",
+  "leprechaun",
+  "bewitch",
+  "realm",
+  "satyr",
+  "golem",
+  "griffin",
+  "nymph",
+  "dragon",
+  "crystal ball",
+  "enchantment",
+  "hobbit",
+  "divination",
+  "portal",
+  "phoenix",
+  "hippogriff",
+  "yeti",
+  "werewolf",
+  "centaur",
+  "pegasus"
+];
+
+export const TotalWords = 319;
+export const TotalLives = 7;
+
+export const getEmoji = (category) => {
+  let emoji = "";
+  switch (category) {
+    case "Colors":
+      emoji = " 🎨";
+      break;
+    case "Shapes":
+      emoji = " ⬜️";
+      break;
+    case "Movies":
+      emoji = " 🎥";
+      break;
+    case "Superheroes":
+      emoji = " 🦸";
+      break;
+    case "Countries":
+      emoji = " 🏳️";
+      break;
+    case "Naruto":
+      emoji = " 🌀";
+      break;
+    case "Flowers":
+      emoji = " 🌼";
+      break;
+    case "Disney":
+      emoji = " 🧜‍♀️";
+      break;
+    case "HarryPotter":
+      emoji = " 🧙";
+      break;
+    case "Music":
+      emoji = " 🎸";
+      break;
+    case "Fantasy":
+      emoji = " 🦄";
+      break;
+    default:
+      emoji = [];
+  }
+  return emoji;
+};
