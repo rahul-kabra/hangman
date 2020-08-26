47cc1e5f812b58ee722740b407f1ba742d8bc88d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-26 13:35:50 +0000

    Deploy rahul-kabra/hangman to github.com/rahul-kabra/hangman.git:gh-pages

---
+++ precache-manifest.bdbe3a98c23eec3b4b0df741695b0d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9304e9a7deb58354e7869cf71056d233",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "ad63ab164274a4061738",
+    "url": "/hangman/static/css/main.9530d458.chunk.css"
+  },
+  {
+    "revision": "636a535ad62746942850",
+    "url": "/hangman/static/js/2.79537edc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.79537edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad63ab164274a4061738",
+    "url": "/hangman/static/js/main.56ab42d7.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
